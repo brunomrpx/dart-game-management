6bdad47617dd861640c717564dfc2e74bc1c4451
Bruno Paixao <brunomrpx@gmail.com>
2016-11-04 01:45:46 -0200

    Adjustments in code structure

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,45 +1,42 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var users = [
+let users = [
   { id: 1, name: 'Bruno', username: 'brunomrpx', email: 'brunomrpx@gmail.com'},
   { id: 2, name: 'Anna', username: 'anna', email: 'anna@gmail.com'},
   { id: 3, name: 'Jairo', username: 'jax', email: 'jairo@gmail.com'}
 ];
 
-router.get('/', function(req, res, next) {
-  var parameters = {
+router.get('/', (req, res, next) => {
+  let parameters = {
     users: users
   };
-
-  console.log('parameters: ', parameters);
 
   res.render('users/index', parameters);
 });
 
-router.get('/new', function(req, res, next) {
+router.get('/new', (req, res, next) => {
   let parameters = {
     user: {}
   };
 
-  res.render('users/new', parameters);
+  res.render('users/form', parameters);
 });
 
-router.post('/new', function(req, res, next) {
+router.post('/save/:id?', (req, res, next) => {
+  let userId = req.params.id;
   let userData = req.body;
 
-  console.log('-> userData: ', userData);
-
   // adding new user
-  if (!userData.id) {
+  if (!userId) {
     let userIds = users.map(u => u.id);
     let nextId = Math.max.apply(null, userIds);
     userData.id = ++nextId;
     users.push(userData);
   } else {
-    let user = users.find(u => u.id == userData.id);
+    let user = users.find(u => u.id == userId);
     Object.assign(user, userData);
   }
 
@@ -54,7 +51,7 @@ router.get('/edit/:id', (req, res, next) => {
     user: user
   };
 
-  res.render('users/new', parameters);
+  res.render('users/form', parameters);
 });
 
 router.get('/remove/:id', (req, res, next) => {
